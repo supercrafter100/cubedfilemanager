3019e3054e4090d0a88e1e9198012a83bc2bffac
PondWader <66561610+PondWader@users.noreply.github.com>
2021-11-03 19:25:56 +0000

    Set isGlobal on update-notifier to true

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import minimist from 'minimist';
  * Notify users if a new update is ready
  */
 const pkg = JSON.parse(readFileSync(join(__dirname, '../package.json'), 'utf8'));
-update_notifier({ pkg, updateCheckInterval: 0 }).notify();
+update_notifier({ pkg, updateCheckInterval: 0 }).notify({ isGlobal: true });
 
 /**
  * Write a banner saying the package name
