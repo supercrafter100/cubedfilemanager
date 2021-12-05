fe077bec1d96bfa20de18930c468cee7440bafe5
PondWader <66561610+PondWader@users.noreply.github.com>
2021-12-05 20:21:16 +0000

    Make sync use baseDir

--- src/util/syncScriptsToLocal.ts
+++ src/util/syncScriptsToLocal.ts
@@ -2,25 +2,26 @@ import fs from "fs";
 import path from "path";
 import CubedFileManager from "../CubedFileManager.js";
 import fetch from 'node-fetch';
+import { join } from "path";
 
 export default class FileDownloader {
 
 
-    private manager: CubedFileManager;
+    private instance: CubedFileManager;
 
-    constructor(manager: CubedFileManager) {
-        this.manager = manager;
+    constructor(instance: CubedFileManager) {
+        this.instance = instance;
     }
 
     public async downloadFiles(dir: string) {
         // Get all files in the file manager
-        const url = `https://playerservers.com/queries/list_files/?dir=/plugins/Skript/scripts${dir}`;
-        const json: any = await fetch(url, { headers: this.manager.headers as any }).then((res) => res.json());
+        const url = `https://playerservers.com/queries/list_files/?dir=${this.instance.fixPath(join(this.instance.baseDir, dir))}`;
+        const json: any = await fetch(url, { headers: this.instance.headers as any }).then((res) => res.json());
 
         // Looping through all files
         for (const file of json.files) {
-            const contents = await this.manager.requestManager.getFileContent(dir, file.filename);
-            const p = path.join(this.manager.rootDir, '.', dir);
+            const contents = await this.instance.requestManager.getFileContent(dir, file.filename);
+            const p = path.join(this.instance.rootDir, '.', dir);
             
             await fs.promises.mkdir(p, { recursive: true });
             fs.writeFileSync(path.join(p, file.filename), contents);
