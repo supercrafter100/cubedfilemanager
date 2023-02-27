500ec3df088bb448fac7091eb572d5f775de3f44
JeroenoBoy <Jeroen@vandegeest.eu>
2023-02-27 15:35:06 +0100

    Fix formatting in getSkriptErrors.ts
    this formatting makes the code more readable and more maintanable

--- src/util/getSkriptErrors.ts
+++ src/util/getSkriptErrors.ts
@@ -1,12 +1,12 @@
-export default (console_data: string, name: string) => {
+export default function getSkriptErrors(console_data: string, name: string) {
 
 	console_data = console_data
-	.replace(/<br \/>/g, "")
-	.replace(/&quot;/g,'"')
-	.replace(/&amp;/g, '&')
-	.replace(/&lt;/g, '<')
-	.replace(/&gt;/g, '>')
-	.replace(/&#039;/g, '\'')
+		.replace(/<br \/>/g, "")
+		.replace(/&quot;/g,'"')
+		.replace(/&amp;/g, '&')
+		.replace(/&lt;/g, '<')
+		.replace(/&gt;/g, '>')
+		.replace(/&#039;/g, '\'');
 	
 	const lines = console_data.split('\n');
 
@@ -15,26 +15,26 @@ export default (console_data: string, name: string) => {
 	let latest_error_lines = [];
 	let latest_error_amount = 0;
 
-
 	for (let i = 0; i < lines.length; i++) {
-		const current_line = lines[i]
+		const current_line = lines[i];
 		if (current_line.includes(`[Skript] Reloading ${name}...`) && !reading_error) {
 			
 			reading_error = true;
 			latest_error_lines = [];
 			latest_error_amount = 0;
 			latest_error_lines.push(current_line);
-
-		} else if (current_line.includes(`[Skript] Encountered`) && reading_error) {
+		}
+		else if (current_line.includes(`[Skript] Encountered`) && reading_error) {
 			
 			reading_error = false;
 			const split_error_line = current_line.split(' ');
 			const error_count = split_error_line[5];
 			latest_error_amount = parseInt(error_count);
-
-		} else if (current_line.includes(`Successfully reloaded ${name}.`) && reading_error) {
+		}
+		else if (current_line.includes(`Successfully reloaded ${name}.`) && reading_error) {
 			reading_error = false;
-		} else if (reading_error) {
+		}
+		else if (reading_error) {
 			latest_error_lines.push(current_line);
 		}
 	}
